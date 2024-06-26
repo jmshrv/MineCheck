47052404df95e6deaacc84da0b6c944c6a46cfd5
James Harvey <44349936+jmshrv@users.noreply.github.com>
2024-06-26 19:43:03 +0100

    Initial Commit

---
+++ MineCheck/MineCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  MineCheckApp.swift
+//  MineCheck
+//
+//  Created by James on 26/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct MineCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
